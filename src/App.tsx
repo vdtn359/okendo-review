64136b1223dd0545e503f6c0f3aa6cf3803c659a
Tuan Nguyen <tuan.nguyen@hiivesystems.com>
2019-09-17 20:41:49 +1000

    Base css

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
+import './styles.scss';
 import { ReviewForm } from './ReviewForm';
 
 export const App: React.FC<any> = function() {
 	const title = '1815 Rose Gold Chronograph Watc - Brown Croco Strap';
 	return (
-		<>
+		<div className={'container'}>
 			<Helmet>
 				<title>{`Reviewing ${title}`}</title>
 			</Helmet>
 			<ReviewForm title={title} />
-		</>
+		</div>
 	);
 };
